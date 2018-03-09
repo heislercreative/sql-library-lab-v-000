189317a10102f94656354673ef68d6cc70112697
Brennan Heisler <learn@heislercreative.com>
2018-03-09 05:07:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) FROM characters
-  ORDER BY species;"
+  GROUP BY species
+  ORDER BY species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
