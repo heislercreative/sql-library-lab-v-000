74cf7d5f7ab77895ce0dd62026028e0d7d4834d9
Brennan Heisler <learn@heislercreative.com>
2018-03-09 04:28:07 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES
 ("Lord of the Rings", 1, 1),
-("Harry Potter", 2, 2);
+("Chronicles of Narnia", 2, 2);
 
 INSERT INTO subgenres (name) VALUES
 ("medieval"),
@@ -8,14 +8,14 @@ INSERT INTO subgenres (name) VALUES
 
 INSERT INTO authors (name) VALUES
 ("JRR Tolkein"),
-("JK Rowling");
+("CS Lewis");
 
 
 INSERT INTO books (title, year, series_id) VALUES
 ("Fellowship of the Ring", 1954, 1),
 ("The Two Towers", 1954, 1),
 ("Return of the King", 1955, 1),
-("Harry Potter and the Philosopher's Stone", 1997, 2),
+("The Lion, the Witch, and the Wardrobe", 1997, 2),
 ("Harry Potter and the Chamber of Secrets", 1998, 2),
 ("Harry Potter and the Prisoner of Azkaban", 1999, 2);
 
