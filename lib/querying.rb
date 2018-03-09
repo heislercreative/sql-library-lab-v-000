a845c51117f836ee859053070695ff64a195cdba
Brennan Heisler <learn@heislercreative.com>
2018-03-09 05:26:11 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
-  WHERE characters.species = 'human'"
+  WHERE characters.species = 'human'
+  GROUP BY series.title"
 end
 
 def select_character_names_and_number_of_books_they_are_in
