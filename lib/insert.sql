f3245665c49c9f776c3eacbf87d9f619ce8c4900
Brennan Heisler <learn@heislercreative.com>
2018-03-09 04:52:47 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,4 +29,5 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
 ("Trufflehunter", "Have at you!", "badger", 2, 2);
 
 INSERT INTO character_books (book_id, character_id) VALUES
-(1, 1), (2, 1), (3, 1), (1, 2), (1, 3), (2, 3), (3, 3), (1, 4)
+(1, 1), (2, 1), (3, 1), (1, 2), (1, 3), (2, 3), (3, 3), (1, 4),
+(4, 5), (5, 5), (6, 5), (4, 6), (5, 6), (6, 6), 
