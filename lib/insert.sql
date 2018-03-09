aa011d26a03dc444245c506766b64f1208e485ad
Brennan Heisler <learn@heislercreative.com>
2018-03-09 04:16:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,5 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES
 ("Lord of the Rings", 1, 1),
 ("Harry Potter", 2, 2);
 
-INSERT INTO books ()
+INSERT INTO books (title, year, series_id) VALUES
+("Fellowship of the Ring", )
